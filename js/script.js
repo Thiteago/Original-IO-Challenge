4cc59407055a6ef341ec83386a7a30fe6f6e10b8
Thiago David <thiagodavidpd@gmail.com>
2021-07-15 21:15:06 -0300

    insert change the text value for the color selected

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
 function selectColor(){
+    var cor = document.getElementById('cor');
     if(document.getElementById('radio-rosa').checked == true){
         document.getElementById('rosa').classList.add("cor-selecionada")
         document.getElementById('azul').classList.remove("cor-selecionada")
         document.getElementById('marrom').classList.remove("cor-selecionada")
         document.getElementById('preto').classList.remove("cor-selecionada")
+        cor.innerHTML = "(Rosa)";
 
     }else if(document.getElementById('radio-azul').checked == true){
         document.getElementById('azul').classList.add("cor-selecionada")
         document.getElementById('rosa').classList.remove("cor-selecionada")
         document.getElementById('marrom').classList.remove("cor-selecionada")
         document.getElementById('preto').classList.remove("cor-selecionada")
+        cor.innerHTML = "(Azul)";
 
     }else if(document.getElementById('radio-marrom').checked == true){
         document.getElementById('marrom').classList.add("cor-selecionada")
         document.getElementById('rosa').classList.remove("cor-selecionada")
         document.getElementById('azul').classList.remove("cor-selecionada")
         document.getElementById('preto').classList.remove("cor-selecionada")
+        cor.innerHTML = "(Marrom)";
 
     }else if(document.getElementById('radio-preto').checked == true){
         document.getElementById('preto').classList.add("cor-selecionada")
         document.getElementById('rosa').classList.remove("cor-selecionada")
         document.getElementById('azul').classList.remove("cor-selecionada")
         document.getElementById('marrom').classList.remove("cor-selecionada")
+        cor.innerHTML = "(Preto)";
     }
+
+    
 }
 
 
